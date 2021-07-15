155f78ef2c9b1d3082d4f35f67e6c651c94bb70e
InukaAsith <inukaasith7@gmail.com>
2021-07-15 11:00:45 +0000

    Auto Fixes: Code formatted

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import random
 from contextlib import suppress
 
 from aiogram.types.inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup
