a7d641a62772331a24d42bf7a7788973b9f23159
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-11 12:17:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/ZauteKm/Daisy /Daisy
-cd /Media-Search-Bot
+cd /Daisy
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m Daisy
